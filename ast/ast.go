642ca4b0569e93db142b6dc42c9258965abe8e10
Rahul <35rahuldshetty@gmail.com>
2022-07-28 01:09:27 +0530

    added integerliteral

--- ast/ast.go
+++ ast/ast.go
@@ -116,3 +116,13 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+// IntegerLiteral
+type IntegerLiteral struct{
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string { return il.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/rahuldshetty/talion/ast"
 	"github.com/rahuldshetty/talion/lexer"
@@ -52,6 +53,7 @@ func New(l *lexer.Lexer) *Parser{
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifer)	
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	return p
 }
@@ -155,6 +157,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifer() ast.Expression{
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression{
+	lit := &ast.IntegerLiteral{Token:p.curToken}
+	value,err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("Could not parse %q as integer.", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
