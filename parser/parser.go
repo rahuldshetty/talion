a61b64308528a75a40c25c87df72e50cb8bd171b
Rahul <35rahuldshetty@gmail.com>
2022-07-29 20:25:13 +0530

    added fn parameter paring:

--- parser/parser.go
+++ parser/parser.go
@@ -72,6 +72,7 @@ func New(l *lexer.Lexer) *Parser{
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
+	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
 	// binary operatory parsing
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
@@ -304,6 +305,50 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement{
 	return block
 }
 
+// Function
+func (p *Parser) parseFunctionLiteral() ast.Expression{
+	lit := &ast.FunctionLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN){
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE){
+		return nil
+	}
+	
+	lit.Body = p.parseBlockStatement()
+
+	return lit
+}
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier{
+	identifiers := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN){
+		p.nextToken()
+		return identifiers
+	}
+
+	p.nextToken()
+	ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	identifiers = append(identifiers, ident)
+	
+	for p.peekTokenIs(token.COMMA){
+		p.nextToken()
+		p.nextToken()
+		ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+		identifiers = append(identifiers, ident)
+	}
+
+	if !p.expectPeek(token.RPAREN){
+		return nil
+	}
+
+	return identifiers
+}
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
