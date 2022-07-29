d30f33064a7762b1ecfaa9b46e6c7a0d2fd91fd3
Rahul <35rahuldshetty@gmail.com>
2022-07-30 01:05:04 +0530

    added eval code

---
+++ eval/eval.go
@@ -0,0 +1,32 @@
+package eval
+
+import (
+	"github.com/rahuldshetty/talion/ast"
+	"github.com/rahuldshetty/talion/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type){
+		// Statements
+		case *ast.Program:
+			return evalStatements(node.Statements)
+
+		case *ast.ExpressionStatement:
+			return Eval(node.Expression)
+
+		// Expression
+		case *ast.IntegerLiteral: 
+			return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements{
+		result = Eval(statement)		
+	}
+
+	return result
+}
