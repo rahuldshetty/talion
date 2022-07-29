00642c887b2ca1dfbea55a6da2a2ef316579bbd2
Rahul <35rahuldshetty@gmail.com>
2022-07-30 03:25:08 +0530

    added error handling check earlier breaking the flow

--- eval/eval.go
+++ eval/eval.go
@@ -30,6 +30,9 @@ func Eval(node ast.Node) object.Object {
 
 		case *ast.ReturnStatement:
 			val := Eval(node.ReturnValue)
+			if isError(val){
+				return val
+			}
 			return &object.ReturnValue{Value: val}
 
 		// Expression
@@ -41,11 +44,20 @@ func Eval(node ast.Node) object.Object {
 		// Operator Expression
 		case *ast.InfixExpression:
 			left := Eval(node.Left)
+			if isError(left){
+				return left
+			}
 			right := Eval(node.Right)
+			if isError(right){
+				return right
+			}
 			return evalInfixExpression(node.Operator, left, right)
 		
 		case *ast.PrefixExpression:
 			right := Eval(node.Right)
+			if isError(right){
+				return right
+			}
 			return evalPrefixExpression(node.Operator, right)
 
 	}
@@ -89,6 +101,10 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object{
 // If - else Expression
 func evalIfExpression(ie *ast.IfExpression) object.Object{
 	condition := Eval(ie.Condition)
+
+	if isError(condition){
+		return condition
+	}
 
 	if isTruthy(condition){
 		return Eval(ie.Consequence)
@@ -191,4 +207,11 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean{
 
 func newError(format string, a ...interface{}) *object.Error{
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func isError(obj object.Object) bool {
+	if obj != nil{
+		return obj.Type() == object.ERROR_OBJ
+	}
+	return false
 }
