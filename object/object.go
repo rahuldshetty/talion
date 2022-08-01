37a4ea363a2bd9c9be08e3455f5fe716e0945382
Rahul <35rahuldshetty@gmail.com>
2022-08-02 00:16:38 +0530

    added eval support to generate hash map internally

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/rahuldshetty/talion/ast"
@@ -20,6 +21,7 @@ const (
 	FUNCTION_OBJ = "FUNCTION"
 	BUILTIN_OBJ = "BUILTIN"
 	LIST_OBJ = "LIST"
+	HASH_OBJ = "HASH"
 )
 
 // Base Object to represent internal system of interprer - used during runtime evaluation
@@ -126,6 +128,63 @@ func (l *List) Inspect() string{
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
+
+	return out.String()
+}
+
+// HashKey method for String, Boolean and Integer objects
+type HashKey struct{
+	Type ObjectType
+	Value uint64
+}
+
+type Hashable interface{
+	HashKey() HashKey
+}
+
+func (b *Boolean) HashKey() HashKey{
+	var value uint64
+	if b.Value{
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey{
+	return HashKey{Type: i.Type(), Value:uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey{
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type:s.Type(), Value: h.Sum64()}
+}
+
+// Hash
+type HashPair struct{
+	Key Object
+	Value Object
+}
+
+type Hash struct{
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+
+	for _, pair := range h.Pairs{
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
