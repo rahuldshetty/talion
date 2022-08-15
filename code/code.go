bd40a2dfb50331e16f26f7b094fabc0108976dc8
Rahul <35rahuldshetty@gmail.com>
2022-08-15 22:29:47 +0530

    added string method

--- code/code.go
+++ code/code.go
@@ -1,6 +1,7 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
@@ -61,4 +62,58 @@ func Make(op Opcode, operands ...int) []byte{
 
 
 	return instruction
+}
+
+
+func (ins Instructions) String() string{
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins){
+		def, err := Lookup(ins[i])
+		if err != nil{
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, ins[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+
+		i += 1 + read
+	}
+
+	return out.String()
+}
+
+func ReadOperands(def *Definition, ins Instructions) ([]int, int){
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+	for i, width := range def.OperandWidths{
+		switch width{
+			case 2:
+				operands[i] = int(ReadUint16(ins[offset:]))
+		}
+
+		offset += width
+	}
+	return operands, offset
+}
+
+func ReadUint16(ins Instructions) uint16{
+	return binary.BigEndian.Uint16(ins)
+}
+
+func (int Instructions) fmtInstruction(def *Definition, openrands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if operandCount != len(openrands){
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(openrands), operandCount)
+	}
+
+	switch operandCount{
+		case 1:
+			return fmt.Sprintf("%s %d", def.Name, openrands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
 }
--- code/code_test.go
+++ code/code_test.go
@@ -26,3 +26,60 @@ func TestMake(t *testing.T){
 		}
 	}
 }
+
+func TestReadOperands(t *testing.T){
+	tests := []struct{
+		op Opcode
+		operands []int
+		bytesRead int
+	}{
+		{OpConstant, []int{65535}, 2},
+	}
+
+	for _, tt := range tests{
+		instruction := Make(tt.op, tt.operands...)
+
+		def , err := Lookup(byte(tt.op))
+
+		if err != nil{
+			t.Fatalf("definition not found: %q", err)
+		}
+
+		operandsRead, n := ReadOperands(def, instruction[1:])
+		if n != tt.bytesRead{
+			t.Fatalf("n wrong. want=%d got=%d", tt.bytesRead, n)
+		}
+
+		for i, want := range tt.operands{
+			if operandsRead[i] != want{
+				t.Errorf("openrand wrong. want=%d got=%d", want, tt.operands[i])
+			}
+		}
+
+	}
+
+}
+
+
+func TestInstructionsString(t *testing.T){
+	instructions := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),	
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatted := Instructions{}
+	for _, ins := range instructions{
+		concatted = append(concatted, ins...)
+	}
+
+	if concatted.String() != expected{
+		t.Errorf("instruction wrongly formatted.\nwant=%q\ngot=%q", expected, concatted.String())
+	}
+
+}
