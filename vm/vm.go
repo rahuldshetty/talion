907e5250918700fee39596de19d8a4c86375d284
Rahul <35rahuldshetty@gmail.com>
2022-08-16 00:34:48 +0530

    added vm

---
+++ vm/vm.go
@@ -0,0 +1,67 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/rahuldshetty/talion/code"
+	"github.com/rahuldshetty/talion/compiler"
+	"github.com/rahuldshetty/talion/object"
+)
+
+const StackSize = 2048
+
+type VM struct{
+	constants []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp int // point to the next value. top of stack is stack[sp-1]
+}
+
+func New(bytecode *compiler.Bytecode) *VM{
+	return &VM{
+		instructions: bytecode.Instructions,
+		constants: bytecode.Constants,
+
+		stack: make([]object.Object, StackSize),
+		sp: 0,
+	}
+}
+
+
+func (vm *VM) StackTop() object.Object{
+	if vm.sp == 0{
+		return nil
+	}
+	return vm.stack[vm.sp - 1]
+}
+
+func (vm *VM) Run() error {
+	// ip - instruction pointer
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op{
+
+			case code.OpConstant:
+				constIndex := code.ReadUint16(vm.instructions[ip+1:])
+				ip += 2
+				err := vm.push(vm.constants[constIndex])
+				if err != nil{
+					return err
+				}
+
+		}
+	}
+
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize{
+		return fmt.Errorf("Stack Overflow!")
+	}
+	vm.stack[vm.sp] = o
+	vm.sp++
+	return nil
+}
